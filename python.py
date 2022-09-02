d5593c87a4b9db68e4b1f7289fc79e38cf575cdc
shankarjun1997 <109965853+shankarjun1997@users.noreply.github.com>
2022-09-02 21:04:18 +0530

    class & obj

--- python.py
+++ python.py
@@ -1,8 +1,22 @@
-print("hello Blackwins tech")
+# print("hello Blackwins tech")
 
-name= ["Lavanya" , 'RajMohan', "Shankar"]
-m = len(name)
-print(m)
-for i in name :
-    print("{} welcome to blackwins tech ".format(i))
+# name= ["Lavanya" , 'Raj Mohan', "Shankar"]
+# m = len(name)
+# print
+# for i in name :
+#     print("{} welcome to blackwins tech ".format(i))
     
+#class and objects
+class Human :
+    def __init__ (self, name , age ):
+        self.name = name
+        self.age = age
+    def methods (self):
+        print ( " Hi . My name is "+ self.name )
+
+h1 = Human ( " Sherlock " , 60 )
+h1.methods()
+h2 = Human ( " Shankar " , 26 )
+
+
+h2.methods()
